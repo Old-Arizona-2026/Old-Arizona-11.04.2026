5980cd23e8fe93ee4a6c13ad248dca536d789756
MAGINSTER <244345780+MAGINSTER@users.noreply.github.com>
2026-05-20 15:55:46 +0300

    Update 1.4
    The old game HUD (SAMP) balance has been restored.
    
    The client still has a money limit, so amounts above 2 KKK are displayed as negative values. However, I've tried to make it more readable: for example, 3,020,000,000 real money would be displayed as -3,020,000 (in Arizona, amounts under 1,000 virtual money don't matter at all anyway). Just remember: a negative amount means you can add three more zeros to the end.

---
+++ moonloader/oldhudmoney.lua
@@ -0,0 +1,29 @@
+local arz = require "arizona-events"
+local ev = require "samp.events"
+
+function arz.onArizonaDisplay(packet)
+    if string.find(packet.text, "event.player.updateMoney") then
+        print("##MONEY", packet.text)
+        local money = decodeJson(string.match(packet.text, '`(.*)`'))[1]
+        --printStringNow(data,1000)
+        if money < 2000000000 then
+            givePlayerMoney(PLAYER_HANDLE,money - getPlayerMoney(PLAYER_HANDLE))
+        else
+            money = -1 * (money / 1000)
+            givePlayerMoney(PLAYER_HANDLE,money - getPlayerMoney(PLAYER_HANDLE))
+        end
+        --int money = getPlayerMoney(Player player)
+    end
+end
+
+function ev.onResetPlayerMoney()
+    return false
+end
+
+function ev.onGivePlayerMoney(money)
+    return false
+end
+
+function main()
+    wait(-1)    
+end
